3808f4de94e2f93eaf1e1dea6eb86392d9329d2f
Brett Delle Grazie <brett.dellegrazie@indigoblue.co.uk>
2017-07-28 18:18:52 +0100

    note about multi-module caching

--- java/install.sh
+++ java/install.sh
@@ -1,2 +1,4 @@
 #!/bin/bash -x
+# https://issues.apache.org/jira/browse/MDEP-204
+# https://issues.apache.org/jira/browse/MDEP-516
 mvn -B -Dsilent=true -DskipTests=true -DskipITs=true package org.apache.maven.plugins:maven-dependency-plugin:3.0.1:go-offline "$@"
