8f2764c2b89554d65d38111e84d6557c5f12aa93
Brett Delle Grazie <brett.dellegrazie@indigoblue.co.uk>
2017-07-28 18:34:47 +0100

    skip caching of Javadocs too (as per Travis Doc)

--- java/install.sh
+++ java/install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -x
+# Package is required to resolve dependencies properly in mutli-module builds
 # https://issues.apache.org/jira/browse/MDEP-204
 # https://issues.apache.org/jira/browse/MDEP-516
-mvn -B -Dsilent=true -DskipTests=true -DskipITs=true package org.apache.maven.plugins:maven-dependency-plugin:3.0.1:go-offline "$@"
+mvn -B -V -Dsilent=true -DskipTests=true -DskipITs=true -Dmaven.javadoc.skip=true package org.apache.maven.plugins:maven-dependency-plugin:3.0.1:go-offline "$@"
