c5183a6ae4397e31edf0923ff3d92460002c37c5
Brett Delle Grazie <brett.dellegrazie@indigoblue.co.uk>
2017-07-28 13:48:39 +0100

    need to use deploy phase rather than deploy goal

--- java/after_success.sh
+++ java/after_success.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # We only deploy on release tags
-[[ $TRAVIS_TAG == release-* ]] && mvn -DskipTests=true -DskipITs=true -DdeployAtEnd=true -DretryFailedDeploymentCount=2 -B --errors org.apache.maven.plugins:maven-deploy-plugin:2.8.2:deploy
+[[ $TRAVIS_TAG == release-* ]] && mvn -DskipTests=true -DskipITs=true -DdeployAtEnd=true -DretryFailedDeploymentCount=2 -B --errors deploy
