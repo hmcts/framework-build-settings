ae8990ec1e242903997ca2fe121c941a435484e2
Brett Delle Grazie <brett.dellegrazie@indigoblue.co.uk>
2017-07-30 17:29:42 +0100

    java/install.sh: tell maven to include the date/time by default
    - The default format should be milliseconds since start

--- java/install.sh
+++ java/install.sh
@@ -2,4 +2,4 @@
 # Package is required to resolve dependencies properly in mutli-module builds
 # https://issues.apache.org/jira/browse/MDEP-204
 # https://issues.apache.org/jira/browse/MDEP-516
-mvn -B -V -Dsilent=true -DskipTests=true -DskipITs=true -Dmaven.javadoc.skip=true package org.apache.maven.plugins:maven-dependency-plugin:3.0.1:go-offline "$@"
+mvn -B -V -Dsilent=true -DskipTests=true -DskipITs=true -Dorg.slf4j.simpleLogger.showDateTime=true -Dmaven.javadoc.skip=true package org.apache.maven.plugins:maven-dependency-plugin:3.0.1:go-offline "$@"
