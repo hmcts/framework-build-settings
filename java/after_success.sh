95c9795f3c81ca1fd4d19dcb9dd722f1a5694300
allanmckenzie <allan.mckenzie@hmcts.net>
2022-10-18 14:07:37 +0100

    Disable javadoc during release

--- java/after_success.sh
+++ java/after_success.sh
@@ -2,4 +2,4 @@
 # We only deploy on release tags
 TAG_MATCH=${TAG_MATCH:-release-*}
 MVN=$([[ -x ./mvnw ]] && echo "./mvnw" || echo "mvn")
-[[ $TRAVIS_TAG == $TAG_MATCH ]] && $MVN -DskipTests=true -DskipITs=true -Prelease -DdeployAtEnd=true -DretryFailedDeploymentCount=2 -B --errors deploy "$@"
+[[ $TRAVIS_TAG == $TAG_MATCH ]] && $MVN -DskipTests=true -DskipITs=true -Dmaven.javadoc.skip=true -Prelease -DdeployAtEnd=true -DretryFailedDeploymentCount=2 -B --errors deploy "$@"
