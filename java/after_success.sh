25a6377078efc96d4d8e5d1005a2933d4db3c92e
jyothi.balla <jyothi.balla@hmcts.net>
2025-03-25 16:53:34 +0000

    test-build

--- java/after_success.sh
+++ java/after_success.sh
@@ -1,5 +1,4 @@
 #!/bin/bash -x
 # We only deploy on release tags
-TAG_MATCH=${TAG_MATCH:-release-*}
 MVN=$([[ -x ./mvnw ]] && echo "./mvnw" || echo "mvn")
-[[ $TRAVIS_TAG == $TAG_MATCH ]] && $MVN -DskipTests=true -DskipITs=true -Dmaven.javadoc.skip=true -Prelease -DdeployAtEnd=true -DretryFailedDeploymentCount=2 -B --errors deploy "$@"
+$MVN -DskipTests=true -DskipITs=true -Dmaven.javadoc.skip=true -Prelease -DdeployAtEnd=true -DretryFailedDeploymentCount=2 -B --errors deploy "$@"
