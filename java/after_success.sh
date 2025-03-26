8df07188ad963d6f121b4b67d05366b6ad1ca9a4
jyothi.balla <jyothi.balla@hmcts.net>
2025-03-26 14:25:43 +0000

    test-build

--- java/after_success.sh
+++ java/after_success.sh
@@ -2,4 +2,4 @@
 # We only deploy on release tags
 MVN=$([[ -x ./mvnw ]] && echo "./mvnw" || echo "mvn")
 $MVN -DskipTests=true -DskipITs=true -Dmaven.javadoc.skip=true -Pcloudsmith -DdeployAtEnd=true -DretryFailedDeploymentCount=2 -B --errors deploy "$@"
-$MVN -DskipTests=true -DskipITs=true -Dmaven.javadoc.skip=true -Psecurecentral -DdeployAtEnd=true -DretryFailedDeploymentCount=2 -B --errors deploy "$@"
+$MVN -DskipTests=true -DskipITs=true -Dmaven.javadoc.skip=true -Psecurecentral-upload -DdeployAtEnd=true -DretryFailedDeploymentCount=2 -B --errors deploy "$@"
