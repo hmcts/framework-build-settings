d147b9c3f2a00ca23de145efc69291f67c9e759c
jyothi.balla <jyothi.balla@hmcts.net>
2025-03-26 16:33:24 +0000

    test-build

--- java/script-install.sh
+++ java/script-install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -x
 MVN=$([[ -x ./mvnw ]] && echo "./mvnw" || echo "mvn")
-$MVN -B -V install "$@"
+mvn -B -V install "$@"
--- java/script.sh
+++ java/script.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -x
 MVN=$([[ -x ./mvnw ]] && echo "./mvnw" || echo "mvn")
-$MVN -B -V verify "$@"
+mvn -B -V verify "$@"
