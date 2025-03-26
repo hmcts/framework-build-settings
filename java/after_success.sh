9341ae39ea40e40da76d04298a7f6eea7b396547
jyothi.balla <jyothi.balla@hmcts.net>
2025-03-26 17:34:22 +0000

    test-build

--- java/after_success.sh
+++ java/after_success.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -x
 # We only deploy on release tags
 MVN=$([[ -x ./mvnw ]] && echo "./mvnw" || echo "mvn")
-mvn -DskipTests=true -DskipITs=true -Dmaven.javadoc.skip=true -Prelease -DaltDeploymentRepository=securecentral-upload::default::https://libraries-internal.mdv.cpp.nonlive/artifactory/libs-snapshot-sp-azure -DaltDeploymentRepository=cjscommonplatform-maven-repository::default::https://maven.cloudsmith.io/cjscommonplatform/maven-repository -DdeployAtEnd=true -DretryFailedDeploymentCount=2 -B --errors deploy "$@"
+mvn -DskipTests=true -DskipITs=true -Dmaven.javadoc.skip=true -DdeployAtEnd=true -DretryFailedDeploymentCount=2 -B --errors deploy "$@"
 
