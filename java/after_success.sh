1e42207b6ceee16d27deee1aabe0b1659952dfca
jyothi-balla <131781883+jyothi-balla@users.noreply.github.com>
2025-03-27 13:11:51 +0000

    Update settings.xml and script to deploy artifacts to jfrog as well (#2)
    update settings

--- java/after_success.sh
+++ java/after_success.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -x
 # We only deploy on release tags
-TAG_MATCH=${TAG_MATCH:-release-*}
 MVN=$([[ -x ./mvnw ]] && echo "./mvnw" || echo "mvn")
-[[ $TRAVIS_TAG == $TAG_MATCH ]] && $MVN -DskipTests=true -DskipITs=true -Dmaven.javadoc.skip=true -Prelease -DdeployAtEnd=true -DretryFailedDeploymentCount=2 -B --errors deploy "$@"
+$MVN -DskipTests=true -DskipITs=true -Dmaven.javadoc.skip=true -Prelease -DdeployAtEnd=true -DretryFailedDeploymentCount=2 -B --errors deploy "$@"
+$MVN -DskipTests=true -DskipITs=true -Dmaven.javadoc.skip=true -DaltDeploymentRepository=securecentral-upload::default::${artifactoryServer}/${artifactoryRepo} -DdeployAtEnd=true -DretryFailedDeploymentCount=2 -B --errors deploy "$@"
+
